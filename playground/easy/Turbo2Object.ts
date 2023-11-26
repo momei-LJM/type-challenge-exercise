03f43addba737376a14ca385a9cd71993a92cd56
Ljm <2262754355@qq.com>
2023-11-26 16:16:28 +0800

    init

---
+++ playground/easy/Turbo2Object.ts
@@ -0,0 +1,77 @@
+/*
+  11 - 元组转换为对象
+  -------
+  by sinoon (@sinoon) #简单 #object-keys
+
+  ### 题目
+
+  将一个元组类型转换为对象类型，这个对象类型的键/值和元组中的元素对应。
+
+  例如：
+
+  ```ts
+  const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+  type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/11/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+
+// T[number]获取元组值联合类型
+type TupleToObject<T extends readonly any[]> = {
+  [key in T[number]]: key;
+};
+// as const 的作用：将类型换位不可变的,如将类型写为固定值
+// (string|number)[]  with as const ->[1, "2", 3, "mmm"]
+const t = [1, "2", 3, "mmm"] as const;
+type tt = typeof t;
+type aa = (typeof t)[number];
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+const tupleNumber = [1, 2, 3, 4] as const;
+const sym1 = Symbol(1);
+const sym2 = Symbol(2);
+const tupleSymbol = [sym1, sym2] as const;
+const tupleMix = [1, "2", 3, "4", sym1] as const;
+
+type cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+      }
+    >
+  >,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<
+    Equal<
+      TupleToObject<typeof tupleSymbol>,
+      { [sym1]: typeof sym1; [sym2]: typeof sym2 }
+    >
+  >,
+  Expect<
+    Equal<
+      TupleToObject<typeof tupleMix>,
+      { 1: 1; "2": "2"; 3: 3; "4": "4"; [sym1]: typeof sym1 }
+    >
+  >
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/11/answer/zh-CN
+  > 查看解答：https://tsch.js.org/11/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
